03646c5f4ab193200b5e826e1516259856397481
Odhiambo-Michael-Allan <odhiambomichaelallan@gmail.com>
2024-09-26 12:15:30 +0300

    Latest Change

--- feature/foryou/src/main/java/com/squad/update/feature/foryou/ForYouViewModel.kt
+++ feature/foryou/src/main/java/com/squad/update/feature/foryou/ForYouViewModel.kt
@@ -1,18 +1,4 @@
 package com.squad.update.feature.foryou
 
 import androidx.lifecycle.ViewModel
-import com.squad.update.core.data.repository.UserDataRepository
-import com.squad.update.core.data.repository.UserNewsResourceRepository
-import com.squad.update.core.data.util.SyncManager
-import com.squad.update.core.domain.GetFollowableTopicsUseCase
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 
-@HiltViewModel
-class ForYouViewModel @Inject constructor(
-//    syncManager: SyncManager,
-    userNewsResourceRepository: UserNewsResourceRepository,
-    getFollowableTopics: GetFollowableTopicsUseCase,
-    private val userDataRepository: UserDataRepository,
-) : ViewModel() {
-}
